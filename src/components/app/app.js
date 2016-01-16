c5359c4df85f873117a5d623cf098309759fc8e8
douglasskiser <douglass.kiser@me.com>
2016-01-16 12:19:55 -0600

    added navigation bar changed react/jsx-no-bind eslint value to 0

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,7 +2,10 @@ import React, {Component, PropTypes} from 'react-native';
 import HomeView from '../../views/home-view/home-view';
 
 const {
-  Platform
+  Platform,
+  Navigator,
+  StyleSheet,
+  Text
   } = React;
 
 export default class App extends Component {
@@ -23,14 +26,87 @@ export default class App extends Component {
     };
   }
 
-  render () {
-    const { instructions } = this.props;
-    let { platform } = this.state;
-
+  _renderHomeView() {
+    const {instructions} = this.props;
+    const {platform} = this.state;
     return (
       <HomeView instructions={instructions}
           platform={platform}
       />
     );
   }
+
+  _renderScene(route) {
+    return {
+      home: this._renderHomeView()
+    }[route.name];
+  }
+
+  _renderNavigationBar() {
+    const navigationBarRouteMapper = {
+      LeftButton: () => {
+
+      },
+      RightButton: () => {
+
+      },
+      Title: (route) => {
+        return {
+          home: (
+            <Text style={styles.navBarTitleText}>
+              Welcome
+            </Text>
+          )
+        }[route.name];
+      }
+    };
+    return (
+      <Navigator.NavigationBar routeMapper={navigationBarRouteMapper}
+          style={styles.navBar}
+      />
+    );
+  }
+
+  render () {
+    const initialRoute = {name: 'home'};
+    return (
+      <Navigator initialRoute={initialRoute}
+          navigationBar={this._renderNavigationBar()}
+          renderScene={(route, navigator) => this._renderScene(route, navigator)}
+      />
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  navContainer: {
+    flex: 1
+  },
+  navBar: {
+    backgroundColor: '#3F51B5',
+    borderBottomColor: '#1A237E',
+    borderBottomWidth: 1
+  },
+  navBarTitleText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '500',
+    marginVertical: 9
+  },
+  navBarLeftButton: {
+    paddingLeft: 10
+  },
+  navBarRightButton: {
+    paddingRight: 10
+  },
+  navBarButtonText: {
+    color: '#EEE',
+    fontSize: 16,
+    marginVertical: 10
+  }
+});
